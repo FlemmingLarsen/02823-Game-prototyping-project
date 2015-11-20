2a35bf0b17a366261b4c5528f319063d741ee5ae
Marianne Louis-Hansen <ml-h@hotmail.com>
2015-11-20 10:39:18 +0100

    Bird that moves on keyboard input

---
+++ Hummingbird/Assets/Scripts/BirdControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdControl : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float verticalvel;
+
+	void Awake (){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			rb.velocity = new Vector2(0, verticalvel);
+			            }
+			
+		if (Input.GetKeyDown(KeyCode.DownArrow)){
+				rb.velocity = new Vector2(0,-verticalvel);
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.DownArrow)) {
+			rb.velocity = (new Vector2 (0, 0));
+		}
+
+
+	}
+
+
+}
