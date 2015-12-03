86682c9ee25add9621e9476fc3b2349cf5ad920e
Marianne Louis-Hansen <ml-h@hotmail.com>
2015-12-03 14:16:46 +0100

    Added notes

---
+++ Hummingbird/Assets/Scripts/Note.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+
+	public Vector2 velocity = Vector2.zero;
+	private Rigidbody2D body2d;
+
+	// Use this for initialization
+	void Awake () {
+
+		body2d = GetComponent<Rigidbody2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		body2d.velocity = velocity; 
+	}
+}
---
+++ Hummingbird/Assets/Scripts/NoteSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+	public float delay = 8.0f;
+	public bool isActive = true;
+	public int noteCluster = 5;
+	public float offset = 0.01f;
+
+	public Vector3 spawnPoint;
+	
+	// Use this for initialization
+	void Start () {
+
+		spawnPoint = transform.position;
+		StartCoroutine (NoteGenerator ());
+	
+	}
+
+	IEnumerator NoteGenerator() {
+
+		yield return new WaitForSeconds (delay);
+
+		float notes = noteCluster;
+
+		if (isActive) {
+
+			while (notes >= 0) {
+
+				yield return new WaitForSeconds (delay/2);
+
+				var newTransform = transform;
+
+				Instantiate(prefabs[0], transform.position, Quaternion.identity);
+				transform.position =  new Vector3(transform.position.x, transform.position.y + offset, transform.position.z);
+				notes--;
+
+			}
+			transform.position = spawnPoint;
+		}
+
+
+		StartCoroutine (NoteGenerator ());
+	}
+}
