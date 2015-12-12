a01b8859d1d3d768f8c696f09311a5eae62d6df3
Jonatan Granner Lund <j.gl@hotmail.com>
2015-12-12 14:44:16 +0100

    pushed a merge of the two projects

---
+++ Hummingbird-united/Assets/Scripts/BirdControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdControl : MonoBehaviour {
+
+	private Rigidbody2D bird;
+	public float verticalvel;
+
+	void Awake (){
+		bird = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			bird.velocity = new Vector2(0, verticalvel);
+		}
+			
+		if (Input.GetKeyDown(KeyCode.DownArrow)){
+				 bird.velocity = new Vector2(0,-verticalvel);
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.DownArrow)) {
+			bird.velocity = (new Vector2 (0, 0));
+		}
+
+
+	}
+
+
+}
