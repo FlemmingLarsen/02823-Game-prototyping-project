9645ca3da34ba76f589c227a445350d1a5f714a9
jonatangranner <jonatangranner@gmail.com>
2015-12-18 12:51:19 +0100

    game sounds

---
+++ Hummingbird-united/Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySounds : MonoBehaviour {
+
+    public void flap ()
+    {
+        //Osc send
+        float flapSound = 1.0f;
+        OSCHandler.Instance.SendMessageToClient("SuperCollider", "/flap", flapSound);
+    }
+}
